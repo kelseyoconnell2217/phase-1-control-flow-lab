37a8eb2d871db05479ab127b3ab7f3b03be26493
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2023-12-15 22:47:15 -0500

    Lab Completed

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  let greeting 
+  if(rideDistance <= 400){
+    greeting = 'This one is on me!'
+  } else if(rideDistance > 400 && rideDistance <= 2000){
+    greeting = 'That will be twenty bucks.'
+  } else if(rideDistance > 2000 && rideDistance<= 2500){
+    greeting = 'I will gladly take your thirty bucks.'
+  } else {
+    greeting = 'No can do.'
+  }
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let city
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
+ternaryCheckCity("NYC")
+ternaryCheckCity("Pittsburgh")
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let generosity
+function switchOnCharmFromTip(generosity){
+  switch(generosity){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default : 
+      return 'Bye.'
+  }     
+  return switchOnCharmFromTip
 }
